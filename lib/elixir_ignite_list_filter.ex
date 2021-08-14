7d83cf2dfafd077bf764625b41b284e77e92ef1a
Joao Carvalho <joaokvalho@gmail.com>
2021-08-14 15:11:01 -0300

    first commit

---
+++ lib/elixir_ignite_list_filter.ex
@@ -0,0 +1,18 @@
+defmodule ElixirIgniteListFilter do
+  @moduledoc """
+  Documentation for `ElixirIgniteListFilter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirIgniteListFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_ignite_list_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirIgniteListFilterTest do
+  use ExUnit.Case
+  doctest ElixirIgniteListFilter
+
+  test "greets the world" do
+    assert ElixirIgniteListFilter.hello() == :world
+  end
+end
